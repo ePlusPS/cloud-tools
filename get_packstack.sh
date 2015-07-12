2ec2abd5e9724e2894543c0c9003e15a062c1a4d
Robert Starmer <rstarmer@gmail.com>
2015-07-12 13:46:11 -0700

    add additinoal info for launch a 2 node

--- get_packstack.sh
+++ get_packstack.sh
@@ -7,3 +7,5 @@ yum install -y openstack-packstack
 yum install -y openstack-tools
 
 echo 'Perhaps run the packstack all-in-one installer now?: packstack --allinone'
+echo 'or if you want to have something a little more advanced:'
+echo 'packstack --allinone --default-password='onecloud' --os-compute-hosts=controller_plus_compute_ip,compute_ip'
