2d6dc1247ad6790e687588626289ac69d8f01f92
Robert Starmer <rstarmer@gmail.com>
2015-07-12 14:50:59 -0700

    add more advanced packstack start script

--- get_packstack.sh
+++ get_packstack.sh
@@ -9,3 +9,6 @@ yum install -y openstack-tools
 echo 'Perhaps run the packstack all-in-one installer now?: packstack --allinone'
 echo 'or if you want to have something a little more advanced:'
 echo 'packstack --allinone --default-password='onecloud' --os-compute-hosts=controller_plus_compute_ip,compute_ip'
+echo 'or even more advanced:'
+echo ''
+echo 'packstack --default-password=onecloud --os-neutron-ml2-tenant-network-types=gre --os-compute-hosts=10.1.64.231,10.1.64.241 --provision-all-in-one-ovs-bridge=y --os-neutron-ml2-tenant-network-types=gre --allinone'
