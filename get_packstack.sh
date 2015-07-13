e426f15d43e9de5927e1b6141ccd29431195e48b
Robert Starmer <rstarmer@gmail.com>
2015-07-13 10:59:53 -0700

    Simplifed the output instructions

--- get_packstack.sh
+++ get_packstack.sh
@@ -7,8 +7,7 @@ yum install -y openstack-packstack
 yum install -y openstack-tools
 
 echo 'Perhaps run the packstack all-in-one installer now?: packstack --allinone'
-echo 'or if you want to have something a little more advanced:'
-echo 'packstack --allinone --default-password='onecloud' --os-compute-hosts=controller_plus_compute_ip,compute_ip'
-echo 'or even more advanced:'
+echo ''
+echo 'Or, to also configure the network appropriately, you should run:'
 echo ''
 echo 'packstack --default-password=onecloud --os-neutron-ml2-tenant-network-types=vlan --os-compute-hosts=10.1.64.231,10.1.64.241 --provision-all-in-one-ovs-bridge=y --os-neutron-ml2-type-drivers=flat,vlan --os-neutron-ml2-tunnel-id-ranges=1:1000 --os-neutron-ml2-vlan-ranges=physnet:300:305 --os-neutron-ovs-bridge-mappings=physnet:br-eth1 --os-neutron-ovs-bridge-interfaces=br-eth1:eth1 --os-neutron-lbaas-install=y --os-heat-install=y --allinone'
