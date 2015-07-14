24cf3b8034dbd62a45f83aeb7904c85f70a79627
Robert Starmer <rstarmer@gmail.com>
2015-07-14 13:39:02 -0700

    C

--- get_packstack.sh
+++ get_packstack.sh
@@ -4,7 +4,7 @@ yum update -y
 yum install -y epel-release
 yum install -y https://rdo.fedorapeople.org/rdo-release.rpm
 yum install -y openstack-packstack
-yum install -y openstack-tools
+#yum install -y openstack-tools
 
 echo 'Perhaps run the packstack all-in-one installer now?: packstack --allinone'
 echo ''
